f845e7002096ede6d9b800516cc5b4efabe90e57
Jack Ellis <jackellis1504@gmail.com>
2024-02-28 18:36:29 +0000

    I'm learnding supernintendo chalmers

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
-<script setup>
+<script setup lang="ts">
 import { ref, computed } from "vue";
+import type { Ref } from "vue";
 
 const col1Width = ref(10);
 const otherColWidths = computed(() => (100 - col1Width.value) / 2);
@@ -16,8 +17,120 @@ const itinerary = ref([
     where: ["Swancar Farm", "Trowell Moor", "Trowell", "Nottingham", "NG9 3PQ"],
   },
 ]);
+
+interface MapArea {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  href: string;
+  target: string;
+  alt: string;
+}
+
+const originalImageHeight: number = 1834;
+const originalImageWidth: number = 2798;
+
+const mapAreas: Array<MapArea> = [
+  {
+    x1: 865,
+    y1: 400,
+    x2: 1340,
+    y2: 705,
+    href: "https://www.swancarfarmcountryhouse.com/",
+    target: "_blank",
+    alt: "Swancar Farm Country House",
+  },
+  {
+    x1: 1615,
+    y1: 1150,
+    x2: 2060,
+    y2: 1500,
+    href: "https://beestonparishchurch.com/",
+    target: "_blank",
+    alt: "St John The Baptist Church, Beeston",
+  },
+];
+
+function resizeMap(): Array<MapArea> {
+  const inviteMapImage = document.getElementById("invite-map");
+  if (!inviteMapImage) {
+    return [];
+  }
+
+  const clientWidth = inviteMapImage.clientWidth;
+  const clientHeight = inviteMapImage.clientHeight;
+
+  const xScale = originalImageWidth / clientWidth;
+  const yScale = originalImageHeight / clientHeight;
+
+  const originalAspectRatio = originalImageWidth / originalImageHeight;
+  const newAspectRatio = clientWidth / clientHeight;
+
+  console.table({
+    clientWidth,
+    clientHeight,
+    originalImageWidth,
+    originalImageHeight,
+    xScale,
+    yScale,
+    originalAspectRatio,
+    newAspectRatio,
+  });
+
+  return mapAreas.map(function (area) {
+    const newX1 = area.x1 / xScale;
+    const newX2 = area.x2 / xScale;
+    const newY1 = area.y1 / yScale;
+    const newY2 = area.y2 / yScale;
+    return {
+      ...area,
+      x1: newX1,
+      y1: newY1,
+      x2: newX2,
+      y2: newY2,
+    };
+  });
+}
+
+const computedMapAreas: Ref<Array<MapArea>> = ref([]);
+
+window.onload = function () {
+  computedMapAreas.value = resizeMap();
+};
+
+window.onresize = function () {
+  computedMapAreas.value = resizeMap();
+};
 </script>
 <template>
+  <map name="invite-map">
+    <area
+      v-for="(computedArea, index) in computedMapAreas"
+      :key="index"
+      shape="rect"
+      :coords="
+        [
+          computedArea.x1,
+          computedArea.y1,
+          computedArea.x2,
+          computedArea.y2,
+        ].join(',')
+      "
+      :href="computedArea.href"
+      :alt="computedArea.alt"
+      target="_blank"
+    />
+  </map>
+  <img
+    id="invite-map"
+    src="/img/InviteMap.png"
+    class="w-100"
+    usemap="#invite-map"
+    alt="A map showing the locations for the wedding: St John's Church Beeston and Swancar Farm"
+    :height="originalImageHeight"
+    :width="originalImageWidth"
+  />
   <div class="card">
     <div class="card-header">The Key Information</div>
     <table class="table mb-0">
