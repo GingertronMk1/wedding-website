aae27b8d0362d4c2e77cd150716b2f15ea93bb81
Jack Ellis <jackellis1504@gmail.com>
2024-03-14 17:52:10 +0000

    wip

--- pages/events/the-ceremony.vue
+++ pages/events/the-ceremony.vue
@@ -2,6 +2,9 @@
 <template>
   <h1>The Ceremony</h1>
 
-  <p>The ceremony will be held at St John The Baptist Church in Beeston.</p>
+  <p>
+    The ceremony will be held at St John The Baptist Church in Beeston. It will
+    be presided over by Father Wayne Plimmer.
+  </p>
   <img src="/img/Church PNG.png" width="2484" height="1782" class="w-100" />
 </template>
