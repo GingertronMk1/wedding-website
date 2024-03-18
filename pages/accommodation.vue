e91353fda71c3f6aa5712ff87f44dbf838633f9b
Jack Ellis <jackellis1504@gmail.com>
2024-03-18 18:24:51 +0000

    wip

--- pages/accommodation.vue
+++ pages/accommodation.vue
@@ -4,6 +4,8 @@ interface Option {
   description?: string;
   url: string;
   mapUrl?: string;
+  distanceToChurch: number;
+  distanceToSwancar: number;
 }
 const options: Array<Option> = [
   {
@@ -13,6 +15,8 @@ const options: Array<Option> = [
     url: "https://www.thenottinghambelfry.co.uk",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2402.1564060387423!2d-1.2383456228819782!3d52.98158850195246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879ea45eb8cf975%3A0x73ad946f9869662b!2sDelta%20Hotels%20by%20Marriott%20Nottingham%20Belfry!5e0!3m2!1sen!2suk!4v1710784500915!5m2!1sen!2suk",
+    distanceToChurch: 5.4,
+    distanceToSwancar: 3.1,
   },
   {
     name: "Travelodge Wollaton Park",
@@ -21,6 +25,8 @@ const options: Array<Option> = [
     url: "https://www.travelodge.co.uk/hotels/474/Nottingham-Wollaton-Park-hotel",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2404.483035318741!2d-1.2202679057247763!3d52.9397310333289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879e9eeef211cad%3A0x1953cc88fc4c1627!2sTravelodge%20Nottingham%20Wollaton%20Park!5e0!3m2!1sen!2suk!4v1710784574711!5m2!1sen!2suk",
+    distanceToChurch: 1.8,
+    distanceToSwancar: 2.9,
   },
   {
     name: "Double Tree by Hilton",
@@ -29,6 +35,8 @@ const options: Array<Option> = [
     url: "https://www.hilton.com/en/hotels/emanudi-doubletree-nottingham-gateway/",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2401.9054929988447!2d-1.2036229228817594!3d52.986101201616286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879c1d8030bb3bf%3A0xb0f8a01001770dd0!2sDoubleTree%20by%20Hilton%20Nottingham%20-%20Gateway!5e0!3m2!1sen!2suk!4v1710784654432!5m2!1sen!2suk",
+    distanceToChurch: 6.4,
+    distanceToSwancar: 5,
   },
   {
     name: "Holiday Inn Derby/Nottingham",
@@ -37,6 +45,8 @@ const options: Array<Option> = [
     url: "https://www.hiderbynottinghamhotel.co.uk",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2405.6424907037112!2d-1.304521422884788!3d52.91886310662222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879eed2f1921307%3A0x1f762ac93ec844d!2sHoliday%20Inn%20Derby%20-%20Nottingham%20M1%2C%20JCT.25%2C%20an%20IHG%20Hotel!5e0!3m2!1sen!2suk!4v1710784678492!5m2!1sen!2suk",
+    distanceToChurch: 5.4,
+    distanceToSwancar: 5.8,
   },
   {
     name: "Premier Inn Nottingham West",
@@ -45,6 +55,8 @@ const options: Array<Option> = [
     url: "https://www.premierinn.com/gb/en/hotels/england/nottinghamshire/nottingham/nottingham-west.html",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2401.799589846853!2d-1.2106549228816836!3d52.98800580147451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879ea7a33dbd7f1%3A0xbab36fabb807f25e!2sPremier%20Inn%20Nottingham%20West%20hotel!5e0!3m2!1sen!2suk!4v1710784711641!5m2!1sen!2suk",
+    distanceToChurch: 7.2,
+    distanceToSwancar: 4.7,
   },
   {
     name: "The Star Inn, Beeston",
@@ -53,8 +65,18 @@ const options: Array<Option> = [
     url: "https://www.starbeeston.co.uk/pub-accommodation",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2405.306564790988!2d-1.2157310228844922!3d52.92490970617217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879e9c4ae21d63d%3A0xd8ce4b864324f890!2sThe%20Star%20Inn!5e0!3m2!1sen!2suk!4v1710784740210!5m2!1sen!2suk",
+    distanceToChurch: 0.2,
+    distanceToSwancar: 4.2,
   },
 ];
+
+const computedOptions: Array<Option> = options.toSorted(function (i1, i2) {
+  const differenceChurch = i1.distanceToChurch - i2.distanceToChurch;
+  if (differenceChurch !== 0) {
+    return differenceChurch;
+  }
+  return i1.distanceToSwancar - i2.distanceToSwancar;
+});
 </script>
 <template>
   <div class="container py-3">
@@ -65,13 +87,19 @@ const options: Array<Option> = [
       as straightforward as possible to get somewhere to spend the night.
     </p>
 
-    <section v-for="(item, index) in options" :key="index" class="mb-5 row">
+    <section
+      v-for="(item, index) in computedOptions"
+      :key="index"
+      class="mb-5 row"
+    >
       <div class="col-md-6 col-12 d-flex flex-column justify-content-center">
         <h3 v-text="item.name" />
         <p v-text="item.description" />
+        <span v-text="`Distance to Church: ${item.distanceToChurch} miles`" />
+        <span v-text="`Distance to Swancar: ${item.distanceToSwancar} miles`" />
         <a
           :href="item.url"
-          class="btn btn-primary me-auto"
+          class="btn btn-primary me-auto mt-3"
           target="_blank"
           v-text="`Link`"
         />
@@ -84,6 +112,7 @@ const options: Array<Option> = [
           style="border: 0"
           allowfullscreen="true"
           loading="lazy"
+          class="w-100"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
