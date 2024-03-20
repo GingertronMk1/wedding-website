e12489e60eb5b039c9afb389297b858da43b314e
Jack Ellis <jackellis1504@gmail.com>
2024-03-20 20:03:55 +0000

    Preserve whitespace

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vue: {
+    compilerOptions: {
+      whitespace: "preserve",
+    },
+  },
   runtimeConfig: {
     public: {
       mapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
